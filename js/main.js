285a639202a0abbe2f6a1c54a43aa8d8e18680a7
RayDe00 <kanal.moy@inbox.ru>
2024-02-10 15:04:00 +0700

    2 столбик + редактирование во втором столбике

--- js/main.js
+++ js/main.js
@@ -11,29 +11,48 @@ new Vue({
             },
             tasks: [],
             inProgressTasks: [],
+            inTest: [],
             editingTaskIndex: null,
+            editingColumn: null
         }
     },
     methods: {
         addTask() {
             if (this.editingTaskIndex !== null) {
-                this.tasks[this.editingTaskIndex] = { ...this.newTask, lastEdited: new Date().toISOString().slice(0, 10) };
+                const updatedTask = { ...this.newTask, lastEdited: new Date().toISOString().slice(0, 10) };
+                if (this.editingColumn === 'tasks') {
+                    this.$set(this.tasks, this.editingTaskIndex, updatedTask);
+                } else if (this.editingColumn === 'inProgressTasks') {
+                    this.$set(this.inProgressTasks, this.editingTaskIndex, updatedTask);
+                }
                 this.editingTaskIndex = null;
+                this.editingColumn = null;
             } else {
                 this.tasks.push({ ...this.newTask });
             }
             this.newTask = { title: '', description: '', deadline: '', createdAt: new Date().toISOString().slice(0, 10), lastEdited: null };
         },
-        deleteTask(taskIndex) {
-            this.tasks.splice(taskIndex, 1);
+        deleteTask(taskIndex, column) {
+            if (column === 'tasks') {
+                this.tasks.splice(taskIndex, 1);
+            } else if (column === 'inProgressTasks') {
+                this.inProgressTasks.splice(taskIndex, 1);
+            }
+            this.editingTaskIndex = null;
+            this.editingColumn = null;
         },
-        editTask(taskIndex, updatedTask) {
+        editTask(taskIndex, task, column) {
+            this.newTask = { ...task };
             this.editingTaskIndex = taskIndex;
-            this.newTask = { ...updatedTask };
+            this.editingColumn = column;
         },
         moveToInProgress(taskIndex) {
             const taskToMove = this.tasks.splice(taskIndex, 1)[0];
             this.inProgressTasks.push(taskToMove);
-        }
+        },
+        moveToInTest(taskIndex) {
+            const taskToMove = this.inProgressTasks.splice(taskIndex, 1)[0];
+            this.inTest.push(taskToMove);
+        },
     }
 });
