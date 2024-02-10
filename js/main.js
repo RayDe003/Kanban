19620cf0117a1a60b6ec9ac352dbc3e1458980b2
RayDe00 <kanal.moy@inbox.ru>
2024-02-10 14:24:35 +0700

    редактирование карточки

--- js/main.js
+++ js/main.js
@@ -10,19 +10,26 @@ new Vue({
                 lastEdited: null
             },
             tasks: [],
-            inProgressTasks: []
+            inProgressTasks: [],
+            editingTaskIndex: null,
         }
     },
     methods: {
         addTask() {
-            this.tasks.push({ ...this.newTask });
+            if (this.editingTaskIndex !== null) {
+                this.tasks[this.editingTaskIndex] = { ...this.newTask, lastEdited: new Date().toISOString().slice(0, 10) };
+                this.editingTaskIndex = null;
+            } else {
+                this.tasks.push({ ...this.newTask });
+            }
             this.newTask = { title: '', description: '', deadline: '', createdAt: new Date().toISOString().slice(0, 10), lastEdited: null };
         },
         deleteTask(taskIndex) {
             this.tasks.splice(taskIndex, 1);
         },
         editTask(taskIndex, updatedTask) {
-            this.tasks[taskIndex] = { ...updatedTask, lastEdited: new Date().toISOString().slice(0, 10) };
+            this.editingTaskIndex = taskIndex;
+            this.newTask = { ...updatedTask };
         },
         moveToInProgress(taskIndex) {
             const taskToMove = this.tasks.splice(taskIndex, 1)[0];
